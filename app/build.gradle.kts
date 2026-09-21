af12c67b04ac0acca4c310000e056c9cd894c1ec
TomasThrawat <tomastharwat873@gmail.com>
2026-09-21 06:15:57 +0300

    Use API 36 for CI compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.tomasthrawat.typesafe"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tomasthrawat.typesafe"
         minSdk = 31
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
